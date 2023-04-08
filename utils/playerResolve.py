037ff200d3ea07574a93375da3ccdebe288186ff
Luca Heinrich <Heinrich.Luca@gmx.net>
2023-04-08 22:00:34 +0200

    change prio at PlayerResolve

--- utils/playerResolve.py
+++ utils/playerResolve.py
@@ -13,25 +13,24 @@ class PlayerResolve():
     def getPlayerId(self, input:str):
         """Resolve a player for a given input in the following order:
             1. If input is 4 characters try check for player Id if valid
-            2. Else Try to find with links saved on DB
-            3. Else try to find with playerName"""
+            3. Else try to find with playerName
+            2. Else try to find with links saved on DB"""
 
         #Valid PlayerId (4Digits)
         if input.isdigit() and len(input) == 4:
             playerData = self._db.getPlayerDataById(input)
             if playerData:
                 return input
-        
-        #By Discord name
-        playerId = self._db.getLinkByName(input)
-        if playerId:
-            return playerId
 
         #By pr0game Name
         playerData = self._db.getPlayerDataByName(input)
         if playerData:
             return playerData[1] # ID on pos 1
-        
+
+        #By Discord name
+        playerId = self._db.getLinkByName(input)
+        if playerId:
+            return playerId
 
         
 
